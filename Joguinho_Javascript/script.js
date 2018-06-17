db55a37d33a10dc2d5b406d4961a9b0d56ea90e2
Leticia Ferreira <claire_thousands@hotmail.com>
2018-06-17 15:41:10 -0300

    Add files via upload

---
+++ Joguinho_Javascript/script.js
@@ -0,0 +1,25 @@
+function verificar()
+{
+	var n1 = document.getElementById("n1").innerHTML;
+	var n2 = document.getElementById("n2").value;
+
+	if(n1 == n2)
+	{
+		alert("Você acertou o número!");
+		window.location.href = window.location.href;
+	}
+	else
+	{
+		alert("Você errou!");
+		window.location.href = window.location.href;
+	}
+}
+
+function resetar()
+{
+	document.getElementById("n2").value = "";
+
+	var r = Math.floor(Math.random() * 100);
+
+	document.getElementById("n1").innerHTML = r;
+}
